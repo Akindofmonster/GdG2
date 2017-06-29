dee968e3aaa5bb41fffeb9f266cb66e9dd5fb873
Oliver Härer <oliver.haerer@uni-ulm.de>
2017-06-29 16:50:04 +0200

    Alle Noten hinzugefügt, Zeitabstandsfehler gefixed, flüssigkeit verbessert

--- Programme - Kopie/src/gdg/Equalizer.java
+++ Programme - Kopie/src/gdg/Equalizer.java
@@ -19,242 +19,342 @@ import com.sun.prism.paint.Color;
 /**
  * The Equalizer class is the main controller for the Animation.
  *
- * @author Oliver Härer, Niklas "Lappen" Schmid
+ * @author Oliver Hï¿½rer, Niklas "Lappen" Schmid
  */
 public class Equalizer extends PApplet {
 
-	private static final int Duration = 6000;
-	private static final float AlphaScale = 2.2f;
-	private static int Height = 900;
-	private static int Wide = 1820;
-	private static final int AddScale = (Height + Wide) / 700;
-	private static final float PosFactor = AddScale;
-	// A list of circles to hold the elements to display.
-	private ArrayList<Circle> circles;
-	private ArrayList<Note> notes;
-	int counter = 0;
-	int count = 0;
+  private static final int Duration = 100000;
+  private static final float AlphaScale = 1.2f;
+  private static int Height = 900;
+  private static int Wide = 1820;
+  private static final int AddScale = (Height + Wide) / 700;
+  private static final float PosFactor = AddScale;
+  // A list of circles to hold the elements to display.
+  private ArrayList<Circle> circles;
+  private ArrayList<Note> notes;
+  int counter = 0;
+  int count = 0;
+  long lastMilliseconds = 0;
+  long timer = 0;
+  long time;
 
-	private AudioPlayer song;
-	private FFT fft;
+  private AudioPlayer song;
+  private FFT fft;
 
-	/**
-	 * The settings() method runs before the sketch has been set up, so other
-	 * Processing functions cannot be used at this point.
-	 */
-	@Override
-	public void settings() {
-		fullScreen();
-		Height = displayHeight;
-		Wide = displayWidth;
-		// setSize(Wide, Height);
-		smooth();
-	}
+  /**
+   * The settings() method runs before the sketch has been set up, so other
+   * Processing functions cannot be used at this point.
+   */
+  @Override
+  public void settings() {
+    fullScreen();
+    Height = displayHeight;
+    Wide = displayWidth;
+    // setSize(Wide, Height);
+    smooth();
+  }
 
-	/**
-	 * The setup method is called before displaying any objects. The Method
-	 * calls commands in the Processing API.
-	 */
-	@Override
-	public void setup() {
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		frameRate(1000);
-		background(0);
-		circles = new ArrayList<>();
-		notes = new ArrayList<>();
-		createNotes();
+  /**
+   * The setup method is called before displaying any objects. The Method calls
+   * commands in the Processing API.
+   */
+  @Override
+  public void setup() {
 
-		// float random = random(50, 100);
-		// // Creates random circles to see what an animation could look like
-		//
-		// PVector position = new PVector(this.width / 2, this.height / 2);
-		// for (int i = 0; i < 10; i++) {
-		// float radius = random * i * random(1f, 2);//
-		// random(i*50,i*120);//this.width*i/10/5,
-		// // this.width*i/8);
-		// float weight = 40;// random(25, 35);
-		// float alpha = random(50, 100);
-		// int color = color(random(0, 50f), random(155f, 255f), random(0, 50f),
-		// alpha);
-		// circles.add(new Circle(this, position, radius, weight, color));
-		// }
+    frameRate(1000);
+    background(0);
+    circles = new ArrayList<>();
+    notes = new ArrayList<>();
+    createNotes();
 
-		// song = minim.loadFile("./data/Kontinuum - First Rain.mp3");
-		// fft = new FFT(song.bufferSize(), song.sampleRate());
+    // float random = random(50, 100);
+    // // Creates random circles to see what an animation could look like
+    //
+    // PVector position = new PVector(this.width / 2, this.height / 2);
+    // for (int i = 0; i < 10; i++) {
+    // float radius = random * i * random(1f, 2);//
+    // random(i*50,i*120);//this.width*i/10/5,
+    // // this.width*i/8);
+    // float weight = 40;// random(25, 35);
+    // float alpha = random(50, 100);
+    // int color = color(random(0, 50f), random(155f, 255f), random(0, 50f),
+    // alpha);
+    // circles.add(new Circle(this, position, radius, weight, color));
+    // }
 
-		// spectra = new ArrayList<>();
-		// for (int i = 1; i < 7; i++) {
-		// float radius = i * 50;
-		// float weight = 4;
-		// int color = color(255, 255, random(0, 100), 150);
-		// int side = i % 2 == 0 ? 1 : -1;
-		// PVector orientation = new PVector(side * PConstants.HALF_PI, side);
-		// spectra.add(new Spectrum(this, position, radius, weight, color,
-		// fft.specSize(), orientation));
-		// }
+    // song = minim.loadFile("./data/Kontinuum - First Rain.mp3");
+    // fft = new FFT(song.bufferSize(), song.sampleRate());
 
-		// Start the song right away
-		
-			
-		
+    // spectra = new ArrayList<>();
+    // for (int i = 1; i < 7; i++) {
+    // float radius = i * 50;
+    // float weight = 4;
+    // int color = color(255, 255, random(0, 100), 150);
+    // int side = i % 2 == 0 ? 1 : -1;
+    // PVector orientation = new PVector(side * PConstants.HALF_PI, side);
+    // spectra.add(new Spectrum(this, position, radius, weight, color,
+    // fft.specSize(), orientation));
+    // }
 
-		Minim minim = new Minim(this);
-		song = minim.loadFile("./gdg/objects/03_m___e.s.t___seven_days_of_falling___evening_in_atlantis.mp3");
-		song.play();
+    // Start the song right away
 
-	}
-	//TODO Processing demo library demo particals
-	
-	private void createNotes() {
-		notes.add(new Note(0.068027211, 74, 0.692244898));
-		notes.add(new Note(0.068027211, 69, 0.692244898));
-		notes.add(new Note(0.068027211, 66, 0.692244898));
-		notes.add(new Note(0.068027211, 59, 0.692244898));
-		notes.add(new Note(0.068027211, 62, 0.692244898));
-		notes.add(new Note(0.068027211, 54, 0.692244898));
+    Minim minim = new Minim(this);
+    song = minim.loadFile("./gdg/objects/03_m___e.s.t___seven_days_of_falling___evening_in_atlantis.mp3");
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    song.play();
+    time = System.currentTimeMillis();
 
-		notes.add(new Note(2.274489796, 76, 0.615306122));
-		notes.add(new Note(2.274489796, 64, 0.615306122));
-		notes.add(new Note(2.274489796, 67, 0.615306122));
-		notes.add(new Note(2.274489796, 59, 0.615306122));
-		notes.add(new Note(2.274489796, 69, 0.615306122));
-		notes.add(new Note(2.274489796, 55, 0.615306122));
+  }
+  // TODO Processing demo library demo particals
 
-		notes.add(new Note(5.580045351, 69, 0.954920635));
+  private void createNotes() {
+    notes.add(new Note(0.068027211, 74, 0.692244898));
+    notes.add(new Note(0.068027211, 69, 0.692244898));
+    notes.add(new Note(0.068027211, 66, 0.692244898));
+    notes.add(new Note(0.068027211, 59, 0.692244898));
+    notes.add(new Note(0.068027211, 62, 0.692244898));
+    notes.add(new Note(0.068027211, 54, 0.692244898));
 
-		notes.add(new Note(6.546575964, 74, 1.008616780));
-		notes.add(new Note(6.546575964, 62, 1.008616780));
-		notes.add(new Note(6.546575964, 69, 1.008616780));
-		notes.add(new Note(6.546575964, 59, 1.008616780));
-		notes.add(new Note(6.546575964, 54, 1.008616780));
-		notes.add(new Note(6.546575964, 57, 1.008616780));
+    notes.add(new Note(2.274489796, 76, 0.615306122));
+    notes.add(new Note(2.274489796, 64, 0.615306122));
+    notes.add(new Note(2.274489796, 67, 0.615306122));
+    notes.add(new Note(2.274489796, 59, 0.615306122));
+    notes.add(new Note(2.274489796, 69, 0.615306122));
+    notes.add(new Note(2.274489796, 55, 0.615306122));
 
-		notes.add(new Note(8.790068027, 76, 0.506485261));
-		notes.add(new Note(8.790068027, 64, 0.506485261));
-		notes.add(new Note(8.790068027, 59, 0.506485261));
-		notes.add(new Note(8.790068027, 60, 0.506485261));
+    notes.add(new Note(5.580045351, 69, 0.954920635));
 
-		notes.add(new Note(10.610068027, 60, 0.371519274));
-	}
+    notes.add(new Note(6.546575964, 74, 1.008616780));
+    notes.add(new Note(6.546575964, 62, 1.008616780));
+    notes.add(new Note(6.546575964, 69, 1.008616780));
+    notes.add(new Note(6.546575964, 59, 1.008616780));
+    notes.add(new Note(6.546575964, 54, 1.008616780));
+    notes.add(new Note(6.546575964, 57, 1.008616780));
 
-	/**
-	 * Draw method is responsible for displaying all objects.
-	 */
-	@Override
-	public void draw() {
+    notes.add(new Note(8.790068027, 76, 0.506485261));
+    notes.add(new Note(8.790068027, 64, 0.506485261));
+    notes.add(new Note(8.790068027, 59, 0.506485261));
+    notes.add(new Note(8.790068027, 60, 0.506485261));
 
-		background(0);
-		noStroke();
-		// if (counter <= 0) {
-		// counter = 10;
-		// count++;
-		// System.out.println(count);
-		// fill(color(255, 255, 255, 50));
-		// PShape shape = createShape(PConstants.RECT, 0, 0, Wide, Height);
-		int time = song.position();
+    notes.add(new Note(10.610068027, 60, 0.371519274));
 
-		for (Note n : notes) {
-			if (n.getTimeStamp() >= time - 30&& n.getTimeStamp() <= time + 30&& n.notAdded()) {
-				float randomX = random(Wide / 5, Wide - Wide / 5);
-				float randomY = random(Height / 5, Height - Height / 5);
-				int alpha = 150;
-				int color = 255;
-				circles.add(new Circle(this, new PVector(randomX, randomY), n.GetSize() * 5, 10, color, alpha, n));
-				n.add = true;
-			}
-		}
-		// fft.forward(song.mix);
-		// // Add a jitter variable from fft of the song to the objects
-		// float jitter = fft.getBand(1);
-		// // Display every circle available
-		for (Circle c : circles) {
-			if (c.draw) {
-				transformCircle(time, c);
-				// c.update(jitter);
-				c.display();
-			}
-		}
+    notes.add(new Note(14.037913832, 86, 0.600816327));
+    notes.add(new Note(14.037913832, 69, 0.600816327));
+    notes.add(new Note(14.037913832, 59, 0.600816327));
+    notes.add(new Note(14.037913832, 66, 0.600816327));
+    notes.add(new Note(14.037913832, 62, 0.600816327));
+    notes.add(new Note(14.037913832, 57, 0.600816327));
+    notes.add(new Note(14.037913832, 54, 0.600816327));
 
-		// // update every spectral arc
-		// float val;
-		// for (int i = 0; i < fft.specSize(); i++) {
-		// // update the left spectral arcs
-		// val = song.left.get(i);
-		// for (int j = 1; j < spectra.size(); j += 2) {
-		// spectra.get(j).update(i, val * 40);
-		// }
-		//
-		// // update the right spectral arcs
-		//
-		// for (int j = 0; j < spectra.size(); j += 2) {
-		// spectra.get(j).update(i, val * 40);
-		// }
-		// }
-		//
-		// // display every spectrum available
-		// for (Spectrum s : spectra) {
-		// s.display();
-		// }
-		// } else {
-		// counter--;
-		// for (Circle c : circles) {
-		// c.display();
-		// }
-		// }
-	}
+    notes.add(new Note(16.178503401, 76, 0.300408163));
+    notes.add(new Note(16.178503401, 64, 0.300408163));
+    notes.add(new Note(16.178503401, 67, 0.300408163));
+    notes.add(new Note(16.178503401, 59, 0.300408163));
+    notes.add(new Note(16.178503401, 60, 0.300408163));
 
-	private void transformCircle(int time, Circle c) {
-		float alpha = c.getAlpha() - AlphaScale;
-		c.setColor(c.getColorWithoutAlpha(), alpha);
-		int tempRadius = (int) (c.radius + AddScale);
-		c.scale = tempRadius / c.radius;
-//		float x = c.position.x;
-//		float y = c.position.y;
-//		float x2 = c.position.x;
-//		float y2 = c.position.y;
-//		if (c.position.x >= Wide / 2) {
-//			x += ((x * (c.scale - 1)) / PosFactor);
-//			// System.out.println("x increased");
-//		} else {
-//			x -= ((x * (c.scale - 1)) / PosFactor);
-//			// System.out.println("x decreased");
-//		}
-//		if (c.position.y >= Height / 2) {
-//			y += ((y * (c.scale - 1)) / PosFactor);
-//			// System.out.println("y increased");
-//		} else {
-//			y -= ((y * (c.scale - 1)) / PosFactor);
-//			// System.out.println("y increased");
-//		}
-//		// System.out.println(x + ":" + x2 + " " + y + ":" + y2);
-//		c.position = new PVector(x, y);
-//		if (time - c.node.getTimeStamp() >= Duration) {
-//			c.draw = false;
-//		}
+    notes.add(new Note(20.218775510, 71, 0.963628118));
+    notes.add(new Note(20.218775510, 59, 0.963628118));
+    notes.add(new Note(20.218775510, 64, 0.963628118));
+    notes.add(new Note(20.218775510, 57, 0.963628118));
 
-		// System.out.println("Circle" + c.radius);
-		// fill(c.getColor());
-		noStroke();
-	}
+    notes.add(new Note(25.512925170, 62, 1.179863946));
+    notes.add(new Note(25.512925170, 69, 1.179863946));
+    notes.add(new Note(25.512925170, 57, 1.179863946));
 
-	/**
-	 * Main method to instantiate the PApplet.
-	 *
-	 * @param args
-	 *            Arguments passed to the PApplet.
-	 */
-	public static void main(String[] args) {
-		try {
-			System.setOut(new PrintStream("out.txt"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		PApplet.main(new String[] { Equalizer.class.getName() });
-	}
+    notes.add(new Note(27.514195011, 64, 1.232108844));
+    notes.add(new Note(27.514195011, 76, 1.232108844));
+    notes.add(new Note(27.514195011, 69, 1.232108844));
+    notes.add(new Note(27.514195011, 67, 1.232108844));
+    notes.add(new Note(27.514195011, 59, 1.232108844));
+    notes.add(new Note(27.514195011, 57, 1.232108844));
+
+    notes.add(new Note(31.325170068, 62, 1.560090703));
+    notes.add(new Note(31.325170068, 57, 1.560090703));
+    notes.add(new Note(31.325170068, 69, 1.560090703));
+    notes.add(new Note(31.325170068, 44, 1.560090703));
+    notes.add(new Note(31.325170068, 55, 1.560090703));
+
+    notes.add(new Note(33.711020408, 64, 1.071020408));
+    notes.add(new Note(33.711020408, 62, 1.071020408));
+    notes.add(new Note(33.711020408, 76, 1.071020408));
+    notes.add(new Note(33.711020408, 59, 1.071020408));
+    notes.add(new Note(33.711020408, 55, 1.071020408));
+    notes.add(new Note(33.711020408, 43, 1.071020408));
+
+    notes.add(new Note(37.831111111, 59, 1.114557823));
+    notes.add(new Note(37.831111111, 62, 1.114557823));
+    notes.add(new Note(37.831111111, 69, 1.114557823));
+    notes.add(new Note(37.831111111, 50, 1.114557823));
+    notes.add(new Note(37.831111111, 43, 1.114557823));
+
+    notes.add(new Note(38.584716553, 62, 1.191473923));
+
+    notes.add(new Note(40.177777778, 76, 0.779319728));
+    notes.add(new Note(40.177777778, 69, 0.779319728));
+    notes.add(new Note(40.177777778, 64, 0.779319728));
+    notes.add(new Note(40.177777778, 71, 0.779319728));
+
+    notes.add(new Note(44.823219955, 76, 0.920090703));
+    notes.add(new Note(44.823219955, 71, 0.920090703));
+    notes.add(new Note(44.823219955, 64, 0.920090703));
+    notes.add(new Note(44.823219955, 59, 0.920090703));
+  }
+
+  /**
+   * Draw method is responsible for displaying all objects.
+   */
+  @Override
+  public void draw() {
+
+    background(0);
+    noStroke();
+    // if (counter <= 0) {
+    // counter = 10;
+    // count++;
+    // System.out.println(count);
+    // fill(color(255, 255, 255, 50));
+    // PShape shape = createShape(PConstants.RECT, 0, 0, Wide, Height);
+    // int time = song.position();
+    // if (frameCount % 1000 == 0) {
+    timer += (System.currentTimeMillis() - this.time);
+    this.time = System.currentTimeMillis();
+    println(frameRate + " Framedifference " + (timer - lastMilliseconds) + "   Time "
+        + timer/* / (millis() / 1000f) */);
+    lastMilliseconds = timer;
+    // }
+    for (Note n : notes) {
+      // println("Note with timestamp " + n.timestamp + " lastadded " +
+      // n.getLastadded());
+      if (n.getTimeStamp() >= timer - 10 && n.getTimeStamp() <= timer + 5 && n.notAdded(timer)) {
+        // println("ZeitAbstand: " + n.getTimeStamp() + ":" + timer);
+        float randomX = random(Wide / 5, Wide - Wide / 5);
+        float randomY = random(Height / 5, Height - Height / 5);
+        int alpha = 150;
+        int color = 255;
+        circles.add(new Circle(this, new PVector(randomX, randomY), n.GetSize() * 5, 10, color, alpha, n));
+        System.out.println("Circle added");
+        n.setLastadded(timer);
+      }
+    }
+    // fft.forward(song.mix);
+    // // Add a jitter variable from fft of the song to the objects
+    // float jitter = fft.getBand(1);
+    // // Display every circle available
+    for (Circle c : circles) {
+      if (c.draw) {
+        transformCircle(timer, c);
+        // c.update(jitter);
+        c.display();
+      }
+    }
+
+    // // update every spectral arc
+    // float val;
+    // for (int i = 0; i < fft.specSize(); i++) {
+    // // update the left spectral arcs
+    // val = song.left.get(i);
+    // for (int j = 1; j < spectra.size(); j += 2) {
+    // spectra.get(j).update(i, val * 40);
+    // }
+    //
+    // // update the right spectral arcs
+    //
+    // for (int j = 0; j < spectra.size(); j += 2) {
+    // spectra.get(j).update(i, val * 40);
+    // }
+    // }
+    //
+    // // display every spectrum available
+    // for (Spectrum s : spectra) {
+    // s.display();
+    // }
+    // } else {
+    // counter--;
+    // for (Circle c : circles) {
+    // c.display();
+    // }
+    // }
+
+    // stroke(255);
+    //
+    // for (int i = 0; i < song.bufferSize() - 1; i++) {
+    // line(i, 50 + song.left.get(i) * 50, i + 1, 50 + song.left.get(i + 1) *
+    // 50);
+    // line(i, 150 + song.right.get(i) * 50, i + 1, 150 + song.right.get(i + 1)
+    // * 50);
+    // }
+    //
+    // stroke(255, 0, 0);
+    // float position = map(song.position(), 0, song.length(), 0, width);
+    // line(position, 0, position, height);
+    //
+    // text("Click anywhere to jump to a position in the song.", 10, 20);
+  }
+
+  @Override
+  public void mousePressed() {
+    // choose a position to cue to based on where the user clicked.
+    // the length() method returns the length of recording in milliseconds.
+    int position = (int) (map(mouseX, 0, width, 0, song.length()));
+    for (Note n : notes) {
+      n.setLastadded(-60);
+    }
+    timer = position;
+    song.cue(position);
+  }
+
+  private void transformCircle(long time2, Circle c) {
+    float alpha = c.getAlpha() - AlphaScale;
+    c.setColor(c.getColorWithoutAlpha(), alpha);
+    int tempRadius = (int) (c.radius + AddScale);
+    c.scale = tempRadius / c.radius;
+    // float x = c.position.x;
+    // float y = c.position.y;
+    // float x2 = c.position.x;
+    // float y2 = c.position.y;
+    // if (c.position.x >= Wide / 2) {
+    // x += ((x * (c.scale - 1)) / PosFactor);
+    // // System.out.println("x increased");
+    // } else {
+    // x -= ((x * (c.scale - 1)) / PosFactor);
+    // // System.out.println("x decreased");
+    // }
+    // if (c.position.y >= Height / 2) {
+    // y += ((y * (c.scale - 1)) / PosFactor);
+    // // System.out.println("y increased");
+    // } else {
+    // y -= ((y * (c.scale - 1)) / PosFactor);
+    // // System.out.println("y increased");
+    // }
+    // // System.out.println(x + ":" + x2 + " " + y + ":" + y2);
+    // c.position = new PVector(x, y);
+    // if (time - c.node.getTimeStamp() >= Duration) {
+    // c.draw = false;
+    // }
+
+    // System.out.println("Circle" + c.radius);
+    // fill(c.getColor());
+    noStroke();
+  }
+
+  /**
+   * Main method to instantiate the PApplet.
+   *
+   * @param args
+   *          Arguments passed to the PApplet.
+   */
+  public static void main(String[] args) {
+    try {
+      System.setOut(new PrintStream("out.txt"));
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    PApplet.main(new String[] { Equalizer.class.getName() });
+  }
 }
--- Programme - Kopie/src/gdg/objects/Note.java
+++ Programme - Kopie/src/gdg/objects/Note.java
@@ -2,41 +2,51 @@ package gdg.objects;
 
 public class Note {
 
-	private final float maxFre = 86;
-	private final float minFre = 43;
+  private final float maxFre = 86;
+  private final float minFre = 43;
 
-	public double timestamp;
-	public double frequentcy;
-	public double duration;
-	public boolean add;
+  public double timestamp;
+  public double frequentcy;
+  public double duration;
+  private long lastadded = -60;
 
-	public Note(double timestamp, double frequentcy, double duration) {
-		this.timestamp = timestamp;
-		this.frequentcy = frequentcy;
-		this.duration = duration;
-	}
+  public long getLastadded() {
+    return lastadded;
+  }
 
-	public float GetSize() {
+  public void setLastadded(long lastadded) {
+    // System.out.println("LastSetter set to n at time " + timestamp);
+    this.lastadded = lastadded;
+  }
 
-		return (float) (frequentcy - minFre / 2);
+  public Note(double timestamp, double frequentcy, double duration) {
+    this.timestamp = timestamp;
+    this.frequentcy = frequentcy;
+    this.duration = duration;
+  }
 
-	}
+  public float GetSize() {
 
-	public boolean notAdded() {
-		// TODO Auto-generated method stub
-		return !add;
-	}
+    return (float) (frequentcy - minFre / 1.5);
 
-	public int getTimeStamp() {
-		return (int) (timestamp * 1000);
-	}
+  }
 
-	public int getDuration() {
-		return (int) (duration * 1000);
-	}
+  public boolean notAdded(long time) {
+    System.out.println(String.format("(time %s- lastadded %d)      ", time, lastadded)
+        + ((time - lastadded) >= 15 || (time - lastadded) <= -15));
+    return (time - lastadded) >= 15 || (time - lastadded) <= -15;
+  }
 
-	public float getMaxFre() {
-		return maxFre;
-	}
+  public int getTimeStamp() {
+    return (int) (timestamp * 1000);
+  }
+
+  public int getDuration() {
+    return (int) (duration * 1000);
+  }
+
+  public float getMaxFre() {
+    return maxFre;
+  }
 
 }
